f410e726be8a02739fcb1e724ed368f8048b00db
Fabian Becker <fabian.becker@st.oth-regensburg.de>
2024-04-05 14:15:54 +0200

    implemented solution for main loop logic (untested)

--- 01/lab/U1-3/U1-3.c
+++ 01/lab/U1-3/U1-3.c
@@ -29,33 +29,50 @@
 #define TASTER_A4 PIND7
 #define TASTER_A4_PULL_UP PORTD7
 
-inline void getValues(uint8_t*);
+inline void getDipValues(uint8_t*);
 inline void setLEDS(uint8_t *);
-inline void flankDetection(uint8_t *, uint8_t *);
+inline void getButtonValues(uint8_t *, uint8_t *);
 
 int main(void){
-	// Activate Output and init
+	// Activate Output and Input Pullupss and init
 	DDRD |= ((1 << LED0_MODE) | (1 << LED1_MODE) | (1 << LED2_MODE));
 	PORTD &= ~((1 << LED0_VAL) | (1 << LED1_VAL) | (1 << LED2_VAL));
 	PORTD |= ((1 << DIP1_PULL_UP) | (1 << DIP2_PULL_UP) | (1 << DIP3_PULL_UP) | (1 << TASTER_A3_PULL_UP) | (1 << TASTER_A4_PULL_UP));
 	uint8_t count = 7;
-	// Caches previous button presses
+	// Caches previous button presses -- 1 = not pressed; 0 = pressed
 	uint8_t pressed = 0x02;
 	// Flank detection value 0: no flank; 1: flank detected
 	uint8_t values = 0;
-	
-	uint8_t dec = 1;
+	// If the counter is running or not
+	uint8_t running = 1;
 	
 	while(1){
-		_delay_ms(100);
-		//flankDetection(&pressed, &values);
-		getValues(&values);
-		setLEDS(&values);
-		count = (!count) ? 7 : count - 1;
+		start:
+		for (int i = 0; i < 10; i++){
+			getButtonValues(&pressed, &values);
+			// if timer is running and button A3 pressed
+			if(running && (~values) & 0x01){
+				running = 0;
+				getDipValues(&count);
+				setLEDS(&count);
+				goto start
+			} 
+			// if not running and button A4 pressed
+			else if (!running && (~values) & 0x02){
+				running = 1;
+				goto start
+			}
+			_delay_ms(100);
+		}
+
+		if(running){
+			count = (!count) ? 7 : count - 1;
+			setLEDS(&count);
+		}
 	}
 }
 
-inline void getValues(uint8_t *reg) {
+inline void getDipValues(uint8_t *reg) {
 	*reg = (~PIND >> 3); 
 }
 
@@ -68,7 +85,7 @@ inline void setLEDS(uint8_t *values) {
 	PORTD = tmp;
 }
 
-inline void flankDetection(uint8_t *pressed, uint8_t *values) {
+inline void getButtonValues(uint8_t *pressed, uint8_t *values) {
 	uint8_t tmp = (PIND >> 6);
 	// Reset values
 	*values &= ~(0x03);
